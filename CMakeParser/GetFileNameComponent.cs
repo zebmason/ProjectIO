f14bbeaa3a7c125fe71cba2fea9a105ff67b2d2b
Zebedee Mason <zeb@cyclerouter.co.uk>
2020-05-30 06:49:55 +0100

    Add that file back in

---
+++ CMakeParser/GetFileNameComponent.cs
@@ -0,0 +1,40 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="GetFileNameComponent.cs" company="Zebedee Mason">
+//     Copyright (c) 2020 Zebedee Mason.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace CMakeParser.Core
+{
+    using System.Collections.Generic;
+
+    public class GetFileNameComponent : ICommand
+    {
+        private readonly ILogger _logger;
+
+        public GetFileNameComponent(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Initialise(State state)
+        {
+        }
+
+        public void Command(KeyValuePair<string, string> command, State state)
+        {
+            var pair = Utilities.Split(command.Value);
+            if (command.Value.Contains("NAME_WE"))
+            {
+                var line = command.Value.Replace("NAME_WE", string.Empty).Trim();
+                pair = Utilities.Split(line);
+                var name = state.Replace(pair.Value);
+                name = System.IO.Path.GetFileNameWithoutExtension(name);
+                state.Variables["${" + pair.Key + "}"] = name;
+                return;
+            }
+
+            _logger.Unhandled(command, state);
+        }
+    }
+}
