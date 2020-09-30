facf5c3e6d40602ed6d07f6d829633568479dbf8
Zebedee Mason <zeb@cyclerouter.co.uk>
2020-09-30 15:41:34 +0100

    Add post packing instructions

--- CMakeToVisualStudio/pack.bat
+++ CMakeToVisualStudio/pack.bat
@@ -1,3 +1,5 @@
 "C:\Program Files (x86)\NuGet\nuget.exe" pack CMakeToVisualStudio.csproj -IncludeReferencedProjects -properties Configuration=Release
+echo Use 7-Zip to rename lib\net472 to tools\any
+echo Use 7-Zip to edit the nuspec to remove all mentions of files and dependencies
 echo Install-Package ...\CMakeToVisualStudio\ProjectIO.CMakeToVisualStudio.2.0.0.nupkg
 pause -1
--- VisualStudioToCMake/pack.bat
+++ VisualStudioToCMake/pack.bat
@@ -1,3 +1,5 @@
 "C:\Program Files (x86)\NuGet\nuget.exe" pack VisualStudioToCMake.csproj -IncludeReferencedProjects -properties Configuration=Release
+echo Use 7-Zip to rename lib\net472 to tools\any
+echo Use 7-Zip to edit the nuspec to remove all mentions of files and dependencies
 echo Install-Package ...\VisualStudioToCMake\ProjectIO.VisualStudioToCMake.2.0.0.nupkg
 pause -1
